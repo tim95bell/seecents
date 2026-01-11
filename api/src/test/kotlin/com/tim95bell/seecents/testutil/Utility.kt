2828d6e74bb584aa9d9e2cbaf7a784c70f935a46
tim95bell <tim95bell@gmail.com>
2026-01-11 11:08:46 +1100

    refactor: remove mess ide created when refactoring test utility class location

--- api/src/test/kotlin/com/tim95bell/seecents/testutil/Utility.kt
+++ api/src/test/kotlin/com/tim95bell/seecents/testutil/Utility.kt
@@ -3,6 +3,20 @@ package com.tim95bell.seecents.testutil
 import arrow.core.Either
 import arrow.core.NonEmptyList
 import arrow.core.NonEmptySet
+import com.tim95bell.seecents.domain.model.Email
+import com.tim95bell.seecents.domain.model.Group
+import com.tim95bell.seecents.domain.model.GroupCore
+import com.tim95bell.seecents.domain.model.GroupId
+import com.tim95bell.seecents.domain.model.GroupName
+import com.tim95bell.seecents.domain.model.LedgerEntryCore
+import com.tim95bell.seecents.domain.model.LedgerEntryLineCore
+import com.tim95bell.seecents.domain.model.LedgerEntryType
+import com.tim95bell.seecents.domain.model.MoneyAmount
+import com.tim95bell.seecents.domain.model.PasswordHash
+import com.tim95bell.seecents.domain.model.User
+import com.tim95bell.seecents.domain.model.UserCore
+import com.tim95bell.seecents.domain.model.UserId
+import com.tim95bell.seecents.domain.model.UserName
 import org.junit.jupiter.api.Test
 import java.time.Duration
 import java.time.Instant
@@ -37,19 +51,18 @@ val EUR: Currency = Currency.getInstance("EUR")
 val T0: Instant = Instant.parse("2025-01-01T00:00:00Z")
 val T1: Instant = T0.plus(Duration.ofDays(1))
 
-fun testMoney(currency: Currency = AUD, amount: Long = DEFAULT_MONEY_AMOUNT) =
-    _root_ide_package_.com.tim95bell.seecents.domain.model.MoneyAmount(currency, amount)
+fun testMoney(currency: Currency = AUD, amount: Long = DEFAULT_MONEY_AMOUNT) = MoneyAmount(currency, amount)
 
-fun testUserId(id: Int = 1) = _root_ide_package_.com.tim95bell.seecents.domain.model.UserId("u$id")
+fun testUserId(id: Int = 1) = UserId("u$id")
 
-fun testGroupId(id: Int = 1) = _root_ide_package_.com.tim95bell.seecents.domain.model.GroupId("g$id")
+fun testGroupId(id: Int = 1) = GroupId("g$id")
 
-fun testUserCore(name: String = "test", email: String = "test@gmail.com", passwordHash: String = "password"): com.tim95bell.seecents.domain.model.UserCore {
-    return _root_ide_package_.com.tim95bell.seecents.domain.model.UserCore(
-        _root_ide_package_.com.tim95bell.seecents.domain.model.UserName.Companion.fromCanonical(name)
+fun testUserCore(name: String = "test", email: String = "test@gmail.com", passwordHash: String = "password"): UserCore {
+    return UserCore(
+        UserName.fromCanonical(name)
             .assertRight().value,
-        _root_ide_package_.com.tim95bell.seecents.domain.model.Email.Companion.fromCanonical(email).assertRight().value,
-        _root_ide_package_.com.tim95bell.seecents.domain.model.PasswordHash(passwordHash),
+        Email.fromCanonical(email).assertRight().value,
+        PasswordHash(passwordHash),
     )
 }
 
@@ -58,8 +71,8 @@ fun testUser(
     name: String = "test",
     email: String = "test@gmail.com",
     passwordHash: String = "password"
-): com.tim95bell.seecents.domain.model.User {
-    return _root_ide_package_.com.tim95bell.seecents.domain.model.User(
+): User {
+    return User(
         testUserId(id),
         testUserCore(name = name, email = email, passwordHash = passwordHash)
     )
@@ -68,11 +81,11 @@ fun testUser(
 fun testGroup(
     id: Int = 1,
     currency: Currency = AUD,
-    users: NonEmptySet<com.tim95bell.seecents.domain.model.UserId> = NonEmptySet.of(testUserId(1), testUserId(2))
-) = _root_ide_package_.com.tim95bell.seecents.domain.model.Group(
-    testGroupId(id), _root_ide_package_.com.tim95bell.seecents.domain.model.GroupCore(
+    users: NonEmptySet<UserId> = NonEmptySet.of(testUserId(1), testUserId(2))
+) = Group(
+    testGroupId(id), GroupCore(
         currency = currency,
-        name = _root_ide_package_.com.tim95bell.seecents.domain.model.GroupName.Companion.fromCanonical("test")
+        name = GroupName.fromCanonical("test")
             .assertRight().value,
         users = users,
     )
@@ -82,34 +95,34 @@ fun testLine(
     from: Int = 1,
     to: Int = 2,
     amount: Long = DEFAULT_MONEY_AMOUNT
-) = _root_ide_package_.com.tim95bell.seecents.domain.model.LedgerEntryLineCore.Companion.create(
+) = LedgerEntryLineCore.create(
     testUserId(from),
     testUserId(to),
     testMoney(amount = amount)
 )
 
 fun testEntry(
-    type: com.tim95bell.seecents.domain.model.LedgerEntryType = _root_ide_package_.com.tim95bell.seecents.domain.model.LedgerEntryType.Expense,
+    type: LedgerEntryType = LedgerEntryType.Expense,
     createdAt: Instant = T0,
     effectiveAt: Instant = T0,
-    group: com.tim95bell.seecents.domain.model.Group = _root_ide_package_.com.tim95bell.seecents.domain.model.Group(
-        testGroupId(), _root_ide_package_.com.tim95bell.seecents.domain.model.GroupCore(
-            _root_ide_package_.com.tim95bell.seecents.domain.model.GroupName.Companion.fromCanonical("test")
+    group: Group = Group(
+        testGroupId(), GroupCore(
+            GroupName.fromCanonical("test")
                 .assertRight().value,
             AUD,
             NonEmptySet.of(testUserId(1), testUserId(2)),
         )
     ),
-    lines: NonEmptyList<com.tim95bell.seecents.domain.model.LedgerEntryLineCore> = NonEmptyList.of(
-        _root_ide_package_.com.tim95bell.seecents.domain.model.LedgerEntryLineCore.Companion.create(
+    lines: NonEmptyList<LedgerEntryLineCore> = NonEmptyList.of(
+        LedgerEntryLineCore.create(
             testUserId(1),
             testUserId(2),
             testMoney()
         ).assertRight().value
     ),
-    creatorId: com.tim95bell.seecents.domain.model.UserId = testUserId(1),
-): Either<com.tim95bell.seecents.domain.model.LedgerEntryCore.CreateError, com.tim95bell.seecents.domain.model.LedgerEntryCore> {
-    return _root_ide_package_.com.tim95bell.seecents.domain.model.LedgerEntryCore.Companion.create(
+    creatorId: UserId = testUserId(1),
+): Either<LedgerEntryCore.CreateError, LedgerEntryCore> {
+    return LedgerEntryCore.create(
         type,
         group,
         creatorId,
